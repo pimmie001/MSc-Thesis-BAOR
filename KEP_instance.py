dcedce61237a340ff3ee00d3bbef43bd5e2a5f94
Pim <pimvkeulen@gmail.com>
2023-05-14 15:40:15 +0200

    add KEP instance

---
+++ KEP_instance.py
@@ -0,0 +1,15 @@
+class KEP_instance:
+    """"""
+
+    def __init__(self, arcs, nodes, K):
+        self.arcs = arcs # arcs in the graph
+        self.nodes = nodes # nodes
+        self.K = K # maximum allowed cycle size
+
+
+
+class KEP_solution:
+    """"""
+
+    def __init__(self):
+        pass
