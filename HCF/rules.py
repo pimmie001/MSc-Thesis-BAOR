fa71dddaee3fdf93e4e306080fb0b365b27cb570
pimmie001 <pimvkeulen@gmail.com>
2023-06-03 15:01:21 +0200

    add rules (degree)

---
+++ HCF/rules.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def degree(I, type = 'tot', descending = True):
+    """
+    Returns the order (ascending (default) or descending) of the degree of the nodes
+    type:   tot --> total degree,   in --> incoming degree,   out --> outgoing degree
+    """
+
+    if type == 'tot':
+        I.make_pred_list()
+        degree = [len(I.pred_list[i]) + len(I.adj_list[i]) for i in range(I.n)]
+    elif type == 'in':
+        I.make_pred_list()
+        degree = [len(I.pred_list[i]) for i in range(I.n)]
+    elif type == 'out':
+        degree = [len(I.adj_list[i]) for i in range(I.n)]
+
+    A = np.argsort(degree)
+    return A[::-1] if descending else A
+
