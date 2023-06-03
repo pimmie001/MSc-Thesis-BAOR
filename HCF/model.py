c4103db24d5c0636c0b63bf8ed3a2c8e8685bb8f
pimmie001 <pimvkeulen@gmail.com>
2023-06-04 00:13:35 +0200

    model.py: fix size of matrix M + add small documentation

--- HCF/model.py
+++ HCF/model.py
@@ -1,4 +1,5 @@
 ### Model to find min # of half-cycles
+# TODO: test for small example
 
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -7,8 +8,9 @@ from CYCLE.cycle_formulation import *
 
 def get_index_HC(hc, c2i, H_full):
     """
-    Checks if hc has been created before. If so it will return its index.
-    If not, it will add the hc to the list i2c"""
+    First checks if hc has been created before. If so it will return its index and if not it will add the hc to the list i2c
+    Secondly returns the index of the hc
+    """
 
     if hc in c2i:
         return c2i[hc]
@@ -21,13 +23,16 @@ def get_index_HC(hc, c2i, H_full):
 
 def model(I):
     # C = get_cycles(I) # !!!!
-    C = [['A','B','C','D'],['A','B','C'], ['A','D','C','B']] #! test
+    C = [['A','B','C','D'],['A','B','C'], ['D','A','B','C']] #! test
 
-    c2i = {} # halfcycles to index
+    c2i = {} # (half)cycle to index to keep track of cycle indices
     H_full = [] # list with distinct halfcycles (used to retrieve actual cycle in solution)
-    M = np.zeros((len(C),2)) # matrix with indices of the 2 half cycles corresponding to each cycle
+    M = [] # list of dimensions len(c) * #hc * 2 that keeps track of indices of halfcycles; important for the ILP formulation
+
 
     for cycle in C:
+        M_sub = []
+
         if len(cycle) % 2 == 0:
             for i in range(len(cycle)//2):
                 # make half cycles:
@@ -38,8 +43,7 @@ def model(I):
                     hc2 += (cycle[(j+len(cycle)//2)%len(cycle)],)
 
                 # store half_cycles and index:
-                M[i,0] = get_index_HC(hc1, c2i, H_full)
-                M[i,1] = get_index_HC(hc2, c2i, H_full)
+                M_sub.append((get_index_HC(hc1, c2i, H_full), get_index_HC(hc2, c2i, H_full)))
 
         else:
             for i in range(len(cycle)):
@@ -50,13 +54,15 @@ def model(I):
                     hc1 += (cycle[j%len(cycle)],)
                     if j == i + len(cycle)//2 + 1: # hc2 has one element less than hc1
                         continue
-                    hc2 += (cycle[(j+len(cycle)//2+1)%len(cycle)],) 
+                    hc2 += (cycle[(j+len(cycle)//2+1)%len(cycle)],)
 
                 # store half_cycles and index:
-                M[i,0] = get_index_HC(hc1, c2i, H_full)
-                M[i,1] = get_index_HC(hc2, c2i, H_full)
+                M_sub.append((get_index_HC(hc1, c2i, H_full), get_index_HC(hc2, c2i, H_full)))
 
-#     print(H_full)
-#     print(len(H_full))
+        M.append(M_sub)
+
+    # print(M)
+    # print(H_full)
+    # print(len(H_full))
 
 # model(3)
