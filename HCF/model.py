d59807b910f1ac69ed24279a805b44b643141a78
Pim <pimvkeulen@gmail.com>
2023-06-04 13:02:05 +0200

    add ILP model to HCF.model

--- HCF/model.py
+++ HCF/model.py
@@ -22,12 +22,20 @@ def get_index_HC(hc, c2i, H_full):
 
 
 def model(I):
-    # C = get_cycles(I) # !!!!
-    C = [['A','B','C','D'],['A','B','C'], ['D','A','B','C']] #! test
+    """
+    ILP model to determine the minimum amount of half cycles needed to cover for all cycles
+    First determines matrix M, containing indices of unique half cycles and create a list H_full that stores the halfcycles
+    Secondly solves the ILP model
+    """
+
+
+    ##### Preparations
+    C = get_cycles(I)
+    # C = [['A','B','C','D'],['A','B','C'], ['D','A','B','C']] #! test
 
     c2i = {} # (half)cycle to index to keep track of cycle indices
     H_full = [] # list with distinct halfcycles (used to retrieve actual cycle in solution)
-    M = [] # list of dimensions len(c) * #hc * 2 that keeps track of indices of halfcycles; important for the ILP formulation
+    M = [] # list of dimensions len(c) * #hc_pairs * 2 that keeps track of indices of halfcycles; needed for the ILP formulation
 
 
     for cycle in C:
@@ -61,8 +69,58 @@ def model(I):
 
         M.append(M_sub)
 
-    # print(M)
-    # print(H_full)
-    # print(len(H_full))
 
-# model(3)
+
+    ##### The ILP model
+    m = gp.Model('Choose halfcycles ILP model')
+    gp.setParam('LogFile', 'Logfiles/gurobi_choose_hc.log')
+
+
+    n_c = len(C) # number of cycles
+    n_h = len(H_full) # number of halfcycles
+    n_p = sum([len(M[i]) for i in range(len(M))]) # number of HC pairs
+
+
+    ### variables
+    vars_x = [] # store x variables: 1 iff this half cycle is chosen
+    vars_y = [] # store y variables: 1 iff this pair of halfcycles is chosen
+
+    for j in range(n_h):
+        x = m.addVar(vtype = GRB.BINARY, obj = 1, name = f'x_{j}')
+        vars_x.append(x)
+
+    for i in range(n_p):
+        y = m.addVar(vtype = GRB.BINARY, obj = 0, name = f'y_{i}')
+        vars_y.append(y)
+
+
+    ### constraints
+    # at least one HC pair is chosen for every cycle
+    count = 0
+    for k in range(n_c):
+        cycle = C[k]
+
+        # determine set X_k and keep track of count:
+        car_X_k = len(cycle)//2 if len(cycle)%2 == 0 else len(cycle) # cardinality of X_k: depends on length cycle and whether this length is odd or even
+        X_k = range(count, count + car_X_k) # set X_k
+        count += car_X_k
+        print(X_k)
+
+        LHS = []
+        for i in X_k:
+            LHS.append(vars_y[i])
+        m.addConstr(sum(LHS) >= 1)
+
+    # both halfcycle pairs (x) must be chosen if corresponding y is set to 1
+    i = 0
+    for k in range(n_c):
+        for l in range(len(M[k])):
+            m.addConstr(vars_x[M[k][l][0]] + vars_x[M[k][l][1]] >= 2*vars_y[i])
+            i += 1
+
+
+    ### solve model
+    m.write("model choose hc.lp")
+    # m.setParam('OutputFlag', False)
+    m.optimize()
+
