6f5e78c2751fed34ee67faaa1717084518be6deb
Pim <pimvkeulen@gmail.com>
2023-06-26 15:12:09 +0200

    HCF.exhaustive: add random_orders

--- HCF/exhaustive.py
+++ HCF/exhaustive.py
@@ -8,13 +8,29 @@ from HCF import get_half_cycles
 def exhaustive(I):
     """
     Enumerates the half-cycles n! times (for each possible order) and 
-    keeps track of the number of half cycles created by each order
+    keeps track of the number of half-cycles created by each order
     """
 
     A = [] # list with numbers of half-cycles
     for order in it.permutations(range(I.n)):
         J = deepcopy(I)
         J.change_order(order)
+
+        H = get_half_cycles(J)
+        A.append(len(H))
+
+    return A
+
+
+def random_orders(I, num = 100_000):
+    """
+    Will take 'num' random orders and calculate the number of half-cycles needed for each order
+    """
+
+    A = [] # list with number of half-cycles
+    for _ in range(num):
+        J = deepcopy(I)
+        J.change_order(np.random.permutation(I.n))
 
         H = get_half_cycles(J)
         A.append(len(H))
