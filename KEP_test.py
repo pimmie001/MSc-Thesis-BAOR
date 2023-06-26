3bc74cc004f85b7053a5d1b9cb474fed433a34a0
Pim <pimvkeulen@gmail.com>
2023-06-26 14:06:08 +0200

    update KEP_test

--- KEP_test.py
+++ KEP_test.py
@@ -1,24 +1,45 @@
 from HCF.HCF import *
+from HCF.min_hc import *
+from HCF.min_hc_heuristics import *
+from HCF.rules import *
 from CYCLE.cycle_formulation import *
 import time
+import gurobipy as gp
 
 
 
 
-path = 'Instances/small/genjson-1.json'
+# # path = 'Instances/Small/genjson-0.json'
+# path = 'Instances/DGGKMPT/200-8.json'
+# K = 5
+# I = KEP_instance()
+# I.build_json_instance(path)
+# I.set_K(I.n-1)
 
-I = KEP_instance()
-I.build_json_instance(path)
-print(I.adj_list)
 
 
 
 # nodes = ['A', 'B', 'C', 'D']
 # arcs = [('A','B'), ('B','D'), ('D','C'), ('C','A'), ('B','C'), ('C','B')]
-# K = 3
+# K = 4
 
 # I = KEP_instance()
+# I.build_instance(arcs, nodes)
+# I.set_K(K)
+
+# sol = heuristic(I)
+# print(len(sol))
+
+
+
+# nodes = list(range(1,8))
+# arcs = [(1,2),(1,3),(1,4),(2,4),(2,3),(4,5),(5,6),(4,6),(6,7)]
+# arcs = arcs + [(i,j) for (j,i) in arcs]
+# I = KEP_instance()
 # I.build_instance(arcs, nodes, K)
+
+# beta_power_measure(I)
+# print(sum(beta_power_measure(I).values()))
 
 # print(I.adj_matrix)
 # print(I.adj_list)
@@ -35,17 +56,15 @@ print(I.adj_list)
 
 
 
-# path = 'Instances/Kidney Data 00036/00036-00000078.wmd'
-# I = KEP_instance()
-# I.build_KD36_instance(path, 3)
+path = 'Instances/Kidney Data 00036/00036-00000051.wmd'
+I = KEP_instance()
+K = 6
+I.build_KD36_instance(path, K)
 
-# I.change_order(np.random.permutation(I.n))
-
-# solution = HCF(I)
-
-# print(solution.indices)
-# solution.show_cycles()
-
+st = time.time()
+sol = heuristic(I)
+print(len(sol))
+print(time.time() - st)
 
 # st = time.time()
 # solution = HCF(I)
