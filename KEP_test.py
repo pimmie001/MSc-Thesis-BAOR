923d6158ee872a1f4c4da8c7579438847a4aab42
Pim <pimvkeulen@gmail.com>
2023-07-10 13:17:04 +0200

    update KEP test

--- KEP_test.py
+++ KEP_test.py
@@ -1,23 +1,64 @@
-from HCF.HCF import *
+# from HCF.HCF import *
+# from HCF.HCF2 import *
+import pandas as pd
+import matplotlib.pyplot as plt
 from HCF.min_hc import *
 from HCF.min_hc_heuristics import *
 from HCF.rules import *
 from CYCLE.cycle_formulation import *
-import time
-import gurobipy as gp
+from copy import deepcopy
+
+
+from HCF.HCF_new import HCF
+from random_orders import random_orders
 
 
 
+# path = 'Instances/Small/genjson-0.json'
+path = 'Instances/DGGKMPT/200-1.json'
+K = 4
+I = KEP_instance()
+I.build_json_instance(path, K)
 
-# # path = 'Instances/Small/genjson-0.json'
-# path = 'Instances/DGGKMPT/200-8.json'
-# K = 5
-# I = KEP_instance()
-# I.build_json_instance(path)
-# I.set_K(I.n-1)
+k = 50
+number_of_hcs, solve_time = random_orders(I, k)
+
+print(solve_time)
+print(number_of_hcs)
+plt.scatter(number_of_hcs, solve_time)
+plt.title(f"Instance {I.filename}, k = {k}")
+plt.show()
 
 
 
+#####################################
+
+# solution = HCF(I)
+
+# solution2 = HCF(I, method = "min")
+
+# solution3 = HCF(I, method = "heuristic")
+
+# J = KEP_instance()
+# J.build_json_instance(path, K)
+# J.sort(betweenness_centrality(J), 'desc', change=True)
+# solution4 = HCF(J)
+
+# # print(f"value enumeration {solution.LB}")
+# # print(f"value min {solution2.LB}")
+# # print(f"value heuristic {solution3.LB}")
+
+# df = pd.DataFrame(index = ['enumeration', 'min', 'heuristic', 'rule'], columns = ['obj', 'time H', 'time build model', 'time solve model', 'total time'])
+
+
+
+# df.loc['enumeration']   = [solution.LB, solution.time_find_H, solution.time_build_model, solution.runtime, solution.time_find_H + solution.time_build_model + solution.runtime]
+# df.loc['min']           = [solution2.LB, solution2.time_find_H, solution2.time_build_model, solution2.runtime, solution2.time_find_H + solution2.time_build_model + solution2.runtime]
+# df.loc['heuristic']     = [solution3.LB, solution3.time_find_H, solution3.time_build_model, solution3.runtime, solution3.time_find_H + solution3.time_build_model + solution3.runtime]
+# df.loc['rule']          = [solution4.LB, solution4.time_find_H, solution4.time_build_model, solution4.runtime, solution4.time_find_H + solution4.time_build_model + solution4.runtime]
+
+# print('\n')
+# print(df)
 
 # nodes = ['A', 'B', 'C', 'D']
 # arcs = [('A','B'), ('B','D'), ('D','C'), ('C','A'), ('B','C'), ('C','B')]
@@ -32,48 +73,45 @@ import gurobipy as gp
 
 
 
-# nodes = list(range(1,8))
-# arcs = [(1,2),(1,3),(1,4),(2,4),(2,3),(4,5),(5,6),(4,6),(6,7)]
-# arcs = arcs + [(i,j) for (j,i) in arcs]
+
+
+# path = 'Instances/DGGKMPT/200-2.json'
 # I = KEP_instance()
-# I.build_instance(arcs, nodes, K)
-
-# beta_power_measure(I)
-# print(sum(beta_power_measure(I).values()))
-
-# print(I.adj_matrix)
-# print(I.adj_list)
-# print(I.index)
-# print(I.index_inv)
-
-# print()
-# I.change_order([1,3,0,2])
-
-# print(I.adj_matrix)
-# print(I.adj_list)
-# print(I.index)
-# print(I.index_inv)
+# K = 4
+# I.build_json_instance(path, K)
 
 
 
-path = 'Instances/Kidney Data 00036/00036-00000051.wmd'
-I = KEP_instance()
-K = 6
-I.build_KD36_instance(path, K)
-
-st = time.time()
-sol = heuristic(I)
-print(len(sol))
-print(time.time() - st)
-
-# st = time.time()
-# solution = HCF(I)
-# print()
-# solution.show_summary()
-# print(time.time()-st)
 
 
-# st = time.time()
-# solution2 = HCF(I)
-# solution2.show_summary()
-# print(time.time()-st)
+###########################################
+
+# # HCF
+# solution_hcf = HCF(I)
+
+# # Heuristic
+# solution_hc = heuristic(I)
+# solution_heur = HCF2(solution_hc)
+
+# # Min # of half-cycles
+# solution_hc2 = min_hc(I)
+# solution_hcf2 = HCF2(solution_hc2)
+
+# # Rule
+# J = KEP_instance()
+# J.build_json_instance(path, K)
+# measure = betweenness_centrality(J)
+# J.sort(measure, 'desc', change=True)
+# solution_rule = HCF(J)
+
+# print('\n\n')
+# print(f'solution HCF: {solution_hcf.runtime}')
+# print(f'solution heuristic: {solution_heur.runtime}')
+# print(f'solution HCF2 min hc: {solution_hcf2.runtime}')
+# print(f'solution rule: {solution_rule.runtime}\n')
+
+# print(len(get_half_cycles(I)))
+# print(len(solution_hc.indices))
+# print(len(solution_hc2.indices))
+# print(len(get_half_cycles(J)))
+
