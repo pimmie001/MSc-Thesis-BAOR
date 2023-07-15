90376a2da8c092c8259dc18e8199d80f47cd9c64
Pim <pimvkeulen@gmail.com>
2023-07-15 14:32:50 +0200

    random_orders: small documentation

--- random_orders.py
+++ random_orders.py
@@ -1,10 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from HCF.HCF import *
 
 
 def random_orders(I, k = 1000):
-    """TODO"""
+    """For k random orders, finds the number of half-cycles and corresponding ILP solve time for this order"""
     
     number_of_hcs = [] # number of half-cycles
     solve_time = [] # time to solve ILP model 
@@ -16,7 +15,5 @@ def random_orders(I, k = 1000):
         number_of_hcs.append(len(solution.H))
         solve_time.append(solution.runtime)
 
-
     return number_of_hcs, solve_time
-
 
