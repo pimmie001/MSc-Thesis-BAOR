3a2bb9e31c9364b60680c7435a58d85162302da9
Pim <pimvkeulen@gmail.com>
2023-07-22 16:32:38 +0200

    update KEP_test and make_plot

--- make_plot.py
+++ make_plot.py
@@ -1,13 +1,12 @@
 ###! TEST file to compare time vs number of half-cycles for different methods
 from random_orders import *
 import matplotlib.pyplot as plt
-import numpy as np
 from copy import deepcopy
 from HCF.rules import *
 from HCF.HCF import HCF
 
 
-path = 'Instances/DGGKMPT/200-1.json'
+path = 'Instances/DGGKMPT/100-4.json'
 K = 4
 I = KEP_instance()
 I.build_json_instance(path, K)
@@ -18,9 +17,10 @@ I.build_json_instance(path, K)
 plt.title(f"Instance {I.filename}")
 
 ### random orders
-k = 50
+k = 25
 number_of_hcs, solve_time = random_orders(I, k)
 plt.scatter(number_of_hcs, solve_time, label=f'Random orders ({k})', color='blue')
+
 
 ### betweenness centrality
 J = deepcopy(I)
@@ -28,5 +28,19 @@ J.sort(betweenness_centrality(J), change = True)
 solution = HCF(J)
 plt.scatter(len(solution.H), solution.runtime, label='betweenness centrality', color='red') 
 
+
+### min_HC's 
+solution = HCF(I, method = "min")
+plt.scatter(len(solution.H), solution.runtime, label = 'min hc', color = 'green')
+
+
+### heuristics
+solution = HCF(I, method = "heuristic")
+plt.scatter(len(solution.H), solution.runtime, label = 'heuristic', color = 'mediumslateblue')
+solution = HCF(I, method = "heuristic2")
+plt.scatter(len(solution.H), solution.runtime, label = 'heuristic 2', color = 'blueviolet')
+
+
+###
 plt.legend()
 plt.show()
