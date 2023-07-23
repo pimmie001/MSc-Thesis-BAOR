d00c477c1e0e6b7c7cbee9826bf21e9411d68061
pimmie001 <pimvkeulen@gmail.com>
2023-07-23 20:09:39 +0200

    random_orders: add total solve time

--- random_orders.py
+++ random_orders.py
@@ -6,7 +6,8 @@ def random_orders(I, k = 50):
     """For k random orders, finds the number of half-cycles and corresponding ILP solve time for this order"""
     
     number_of_hcs = [] # number of half-cycles
-    solve_time = [] # time to solve ILP model 
+    solve_time = [] # time to solve ILP model
+    total_time = [] # total runtime (build + solve model)
 
     for _ in range(k):
         I.change_order(np.random.permutation(I.n))
@@ -14,6 +15,7 @@ def random_orders(I, k = 50):
 
         number_of_hcs.append(len(solution.H))
         solve_time.append(solution.runtime)
+        total_time.append(solution.total_time)
 
-    return number_of_hcs, solve_time
+    return number_of_hcs, solve_time, total_time
 
