8eda6530f2485e0975a58c22122a495a353f683d
pimmie001 <pimvkeulen@gmail.com>
2023-10-10 02:14:23 +0200

    rules.py: update on closeness centrality (now harmony centrality) and fix in betweenness-K centrality

--- HCF/rules.py
+++ HCF/rules.py
@@ -2,6 +2,7 @@
 #! not necessarily for HCF only
 
 import networkx as nx
+import numpy as np
 
 
 def degree(I, Type = 'tot'):
@@ -23,17 +24,50 @@ def degree(I, Type = 'tot'):
 
 
 
+def floyd(I):
+    floyd = np.full((I.n, I.n), float('inf')) # initialize matrix
+    for i in I.adj_list:
+        for j in I.adj_list[i]:
+            floyd[i,j] = 1 # set direct neighbors distance to 1
+
+    for i in range(I.n):
+        for j in range(I.n):
+            for k in range(I.n):
+                floyd[j,k] = min(floyd[j,k], floyd[j,i] + floyd[i,k])
+
+    return floyd
+
+
+
 def closeness_centrality(I):
-    """The closeness centrality of a node is the sum of the distance to every other node (distance = n if not possible)"""
+    """
+    Returns the "Harmony Centrality Index" (modified version of closeness centrality for unconnected graphs)/
+    The Harmony Centrality Index of node i is the sum (over all j != i) of the recipicle of the distance from i to j.
+    """
 
     I.build_graph()
-    closeness = nx.closeness_centrality(I.G)
-    return [closeness[i] for i in range(I.n)]
+    floyd_mat = floyd(I)
+    closeness = np.zeros(I.n)
+
+    for i in range(I.n):
+        for j in range(I.n):
+            if i != j:
+                closeness[i] += 1/floyd_mat[i,j]
+
+    return closeness
+
+    #! old closeness centrality: 
+    # closeness = nx.closeness_centrality(I.G)
+    # return [closeness[i] for i in range(I.n)]
 
 
 
 def betweenness_centrality(I):
-    """The betweenness centrality of a node is the amount of times the node lies on a shortest path between other nodes"""
+    """
+    Returns the betweenness centrality of all nodes.
+    The betweenness centrality of node l is the sum (over pairs (i,j)) of the ratio of the 
+    number of paths i to j that contain l over the number of paths i to j.
+    """
 
     I.build_graph()
     betweenness = nx.betweenness_centrality(I.G, normalized=False)
@@ -42,17 +76,48 @@ def betweenness_centrality(I):
 
 
 def betweenness_centrality_K(I): # does not seem to work better than betweenness centrality for HCF
-    """The betweenness centrality of a node is the amount of times the node lies on a shortest path of size K or less between other nodes"""
+    """
+    Returns the betweenness-K centrality of all nodes.
+    The betweenness centrality of node l is the sum (over pairs (i,j)) of the ratio of the number of paths i to j of size K 
+    or less that contain l over the number of paths i to j of size K or less from.
+    """
 
     I.build_graph()
-    betweenness_K = [0 for _ in range(I.n)]
-    all_paths = dict(nx.all_pairs_shortest_path(I.G, cutoff = I.K))
 
-    for source in all_paths:
-        for target in all_paths[source]:
-            path = all_paths[source][target]
-            for node in path[1:-1]:
-                betweenness_K[node] += 1
+    # determine all shortest paths of size K or less
+    shortest_paths = {}
+    for i in range(I.n):
+        for j in range(I.n):
+            try:
+                all_shortest_paths = nx.all_shortest_paths(I.G, source=i, target=j)
+                filtered_paths = [path for path in all_shortest_paths if len(path) <= I.K + 1] # only paths of length K or less
+                shortest_paths[(i,j)] = filtered_paths
+            except:
+                shortest_paths[(i,j)] = []
+
+    # calcualte betweenness-K centrality
+    betweenness_K = [0 for _ in range(I.n)]
+    for l in range(I.n):
+        for i in range(I.n):
+            for j in range(I.n):
+                if i != l and l != j:
+                    paths = shortest_paths[(i,j)]
+                    if paths:
+                        count = 0
+                        for path in paths:
+                            count += (l in path)
+                        betweenness_K[l] += count / len(paths)
 
     return betweenness_K
+
+    #! OLD CODE: 
+    # all_paths = dict(nx.all_pairs_shortest_path(I.G, cutoff = I.K))
+
+    # for source in all_paths:
+    #     for target in all_paths[source]:
+    #         path = all_paths[source][target]
+    #         for node in path[1:-1]:
+    #             betweenness_K[node] += 1
+
+    # return betweenness_K
 
