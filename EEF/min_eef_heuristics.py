13ad55077f66840ab928a1b031a2da1684170d2d
Pim <pimvkeulen@gmail.com>
2023-11-17 16:29:29 +0100

    EEF: heuristic 1: improve performance

--- EEF/min_eef_heuristics.py
+++ EEF/min_eef_heuristics.py
@@ -5,6 +5,10 @@ from EEF.min_eef import *
 from CYCLE.CF import get_cycles
 
 
+###########################################
+############### HEURISTIC 1 ###############
+###########################################
+
 
 def arcs_in_cycle(c):
     # given cycle (eg [1,2,3]) returns the arcs in the cycle (eg [(1,2), (2,3), (3,1)])
@@ -18,12 +22,16 @@ def arcs_in_cycle(c):
 def count_nodes(n, C, exclude):
     # count how often nodes occur in cycles that are not in exclude
     count = np.zeros(n)
+    cycles_nodes = [[] for _ in range(n)]
+    indices_cycles = [[] for _ in range(n)]
     for i,cycle in enumerate(C):
         if i in exclude:
             continue
         for node in cycle:
             count[node] += 1
-    return count
+            cycles_nodes[node].append(cycle)
+            indices_cycles[node].append(i)
+    return count, cycles_nodes, indices_cycles
 
 
 
@@ -53,21 +61,16 @@ def heuristic_eef(I):
     added = set()
     while len(added) < len(C): # all cycles need to be able to be made
         ## find most common node left
-        count = count_nodes(I.n, C, added) # count occurences of nodes
+        count, cycles_nodes, indices_cycles = count_nodes(I.n, C, added) # count occurences of nodes
         l = np.argmax(count)
-
+        C_i = cycles_nodes[l]
 
         ## find which cycles include this node
-        C_i = []
-        for i,cycle in enumerate(C):
-            if i in added:
-                continue
-            if l in cycle:
-                added.add(i)
-                C_i.append(cycle)
-                Z[(l,i)] = 1
+        for i in indices_cycles[l]:
+            Z[(l,i)] = 1
+            added.add(i)
 
-        ## activate variables corresponding to cycle
+        ## activate arcs corresponding to chosen cycles
         for cycle in C_i:
             for (a,b) in arcs_in_cycle(cycle):
                 Y[(l,a,b)] = 1
