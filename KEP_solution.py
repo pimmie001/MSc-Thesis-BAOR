8c796a81b6b1424969fed72d0c0b1844be8f7909
pimmie001 <pimvkeulen@gmail.com>
2023-11-25 15:44:51 +0100

    KEP_solution: rm broken feasibility check EEF

--- KEP_solution.py
+++ KEP_solution.py
@@ -3,7 +3,7 @@ class KEP_solution:
     Class for solution for the KEP
         Stores solution properties such as lower/upper bound. Also includes 
         properties of the corresponding ILP model (e.g. number of variables).
-        Can check the feasibility of a solution found by the CF, HCF or (R)EEF
+        Can check the feasibility of a solution found by the CF or HCF
     """
 
 
@@ -26,9 +26,8 @@ class KEP_solution:
             self.feasibility = self.check_feasibility_CF()
         elif self.formulation == 'HCF':
             self.feasibility = self.check_feasibility_HCF()
-        # elif self.formulation == 'EEF':
-        #! DOES NOT WORK
-        #     self.feasibility = self.check_feasibility_EEF()
+        else:
+            print(f"Feasiblity check for {self.formulation} not implemented")
 
 
     def check_feasibility_CF(self):
@@ -113,72 +112,6 @@ class KEP_solution:
         # check if all halfcycles are matched
         if not len(counterparts) == len(chosen_hcycles)/2:
             return False
-
-        # check objective
-        if abs(total_obj - self.LB) > 0.001:
-            return False
-
-        return True
-
-
-    #! NEEDS UPDATE
-    def check_feasibility_EEF(self):
-        """Checks feasibility of the EEF solution"""
-
-        ### extract solution from xvalues
-        if self.formulation == 'EEF':
-            self.chosen_cycles = []
-            for values in self.xvalues:
-                chosen_arcs = []
-                for i,x in enumerate(values):
-                    if x > 0.5:
-                        chosen_arcs.append(self.I.A[i])
-                if chosen_arcs:
-                    self.chosen_cycles.append(chosen_arcs)
-
-        elif self.formulation == 'REEF':
-            self.chosen_cycles = [[] for _ in range(self.I.n)]
-            for key, index in self.arc_to_index.items():
-                if self.xvalues[index] > 0.5:
-                    self.chosen_cycles[key[0]].append((key[1], key[2]))
-            self.chosen_cycles = [x for x in self.chosen_cycles if x] # remove empty lists
-
-
-        ### check feasibility
-        total_obj = 0
-        chosen_nodes = set()
-
-        for cycle in self.chosen_cycles:
-            # check cycles not larger than K
-            if len(cycle) > self.I.K: 
-                return False
-
-            # check if cycle are actually cycles
-            arc = cycle[0]
-            for _ in range(len(cycle)-1):
-                if not self.I.adj_matrix[arc[0], arc[1]]: # check if arcs exists
-                    return False
-
-                found_arc = False
-                for new_arc in cycle[1:]:
-                    if arc[1] == new_arc[0]:
-                        arc = new_arc
-                        found_arc = True
-                        break
-                if not found_arc:
-                    return False # cant complete cycle
-
-            if not (self.I.adj_matrix[arc[0], arc[1]] and arc[1] == cycle[0][0]): # check if cycle can be completed
-                return False
-
-            # check if nodes not already in another cycle
-            for arc in cycle:
-                if arc[0] in chosen_nodes:
-                    return False
-                chosen_nodes.add(arc[0])
-
-            # keep track on objective
-            total_obj += len(cycle)
 
         # check objective
         if abs(total_obj - self.LB) > 0.001:
